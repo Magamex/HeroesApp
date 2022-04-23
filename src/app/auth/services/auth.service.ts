4205da5715dbbe84eb447c2e1447e7500a574453
LAPTOP-PJ1275UD\Matias <matias.angeluk@gmail.com>
2022-04-23 00:19:27 -0300

    auth.guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { map, tap, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,34 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth():Auth{
+    return {...this._auth!};  //Decir que siempre tiene un valor !
+  }
 
   constructor( private http: HttpClient) { }
 
+  verificaAuthenticacion(): Observable<boolean>{
+    if(!localStorage.getItem('token')){
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+                  .pipe(
+                    map( auth => {
+                      this._auth = auth
+                      return true;
+                    })
+                  )
+  }
+
   login(){
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+                  .pipe(
+                    tap( auth => this._auth = auth),
+                    tap( auth => localStorage.setItem('token', auth.id))
+                  );
   }
 
 }
